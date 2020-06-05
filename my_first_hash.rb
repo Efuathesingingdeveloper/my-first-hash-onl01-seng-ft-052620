843caad1b9ac6b84f0fafee523fc372b480d25b1
Georgette Class-Peters <georgycp@udel.edu>
2020-06-05 19:04:19 +0000

    Automatically backed up by Learn

---
+++ my_first_hash.rb
@@ -0,0 +1,29 @@
+def new_hash
+
+  hash = Hash.new
+  end	
+end
+
+
+def actor	def actor
+
+  hash = {:name => 'Dwayne The Rock Johnson'}
+  end	
+end
+
+
+def monopoly	def monopoly
+	monopoly = {}		monopoly = {:railroads => {}}
+
+
+  end
+end
+
+
+def monopoly_with_second_tier	def monopoly_with_second_tier
+
+	monopoly = {:railroads => {:pieces => 4, :names => {} ,:rent_in_dollars =>{}}
+}
+  end	
+end
+
