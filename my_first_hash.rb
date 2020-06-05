9ac6fb2323aae49dfbd6e371b7b2ab05095afa5b
Georgette Class-Peters <georgycp@udel.edu>
2020-06-05 19:06:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def new_hash
-hash = Hash.new
+  hash = Hash.new
   end	
 end
 
@@ -23,8 +23,7 @@ def monopoly_with_second_tier	def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	
-		  monopoly = {:railroads => {:pieces => 4, :names => {:reading_railroad =>{},:pennsylvania_railroad =>{},:b_and_o_railroad =>{},:shortline =>{}} , :rent_in_dollars => {:one_piece_owned => 25 ,:two_pieces_owned => 50,:three_pieces_owned => 100,
+	monopoly = {:railroads => {:pieces => 4, :names => {:reading_railroad =>{},:pennsylvania_railroad =>{},:b_and_o_railroad =>{},:shortline =>{}} , :rent_in_dollars => {:one_piece_owned => 25 ,:two_pieces_owned => 50,:three_pieces_owned => 100,
         :four_pieces_owned => 200}}
 }
   end	
@@ -38,12 +37,3 @@ def monopoly_with_fourth_tier	def monopoly_with_fourth_tier
 }
   end	
 end
-
-
-
-
-
-
-
-
- 
